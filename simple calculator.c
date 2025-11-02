ba7042c1d0004f5c95464cd7bac8898372b9378a
V Tina Priyadharshini Vel <tinapriyadharshinivel@gmail.com>
2025-11-02 21:25:36 +0530

    Add files via upload

---
+++ simple calculator.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float num1,num2,result;
+    char op;
+
+printf("simple calculator using switch-case\n");
+    printf("-------------------\n");
+    printf("enter an operator(+,-,*,/):");
+    scanf("%c",&op);
+    printf("enter two numbers:");
+    scanf("%f%f",&num1,&num2);
+switch(op){
+    case'+':
+        result=num1+num2;
+        printf("result=%.2f\n",result);
+        break;
+    case'-':
+        result=num1-num2;
+        printf("result=%.2f\n",result);
+        break;
+    case'*':
+        result=num1*num2;
+        printf("result=%.2f\n",result);
+        break;
+    case'/':
+        if(num2!=0)
+            printf("result=%.2f\n",num1/num2);
+        else
+            printf("error! division by zero is not allowed.\n");
+        break;
+    default:
+        printf("invaild operator! please use +,-,*,or/.\n");
+}
+return 0;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
